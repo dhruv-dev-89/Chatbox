5239f32948fe0db8a412fb5cbfc252c1e8400cfb
Dhruv-dev-89 <dhruvimsec10@gmail.com>
2026-07-19 09:52:03 +0530

    changes in backend

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SearchBar = () => {
   return (
     <div className='mt-5 h-[15%] flex flex-col gap-5 items-center'>
-        <h2 className='self-start ml-17 text-white text-4xl font-extrabold'>Chat App</h2>
+        <h2 className='self-start ml-17 text-white text-4xl font-extrabold'>Chat Box</h2>
         <input className='w-[70%] rounded-xl py-1 border-2 border-gray-400 text-gray-100 px-2' type="text"  placeholder='Search...'/>
     </div>
   )
